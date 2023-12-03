beac571f002e3fcda9e5107a390f3c5d92ac8b21
ShaidR <ShaidRojani@gmail.com>
2023-12-03 14:21:07 -0800

    Created an Enemy Spawner

---
+++ Survivor/scenes/enemy.gd
@@ -0,0 +1,11 @@
+class_name Enemy
+extends Unit
+
+@export var movement_speed = 40.0
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
